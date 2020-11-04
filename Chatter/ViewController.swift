d890cd86490cb3ddf033f7d6c6d1693a6b453644
Eleirold Asuncion <emasuncion.dev@gmail.com>
2020-11-04 19:52:20 +0800

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Errol Asuncion on 11/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
